bae5850bd7a428267b4645ef4c8622d48f88f833
Carlos Benavides-Riveros <cabenavir@gmail.com>
2022-05-20 09:19:00 +0200

    Problem 1

--- example.jl
+++ example.jl
@@ -1,13 +1,65 @@
-using LinearAlgebra
+using LinearAlgebra, Polynomials, SpecialPolynomials
+using QuadGK, Calculus, CalculusWithJulia
 
-function f(x)
-   x += 3
+Max = 10
+Q = 1
+z = 1
+a1 = 1
+a2 = 1 
+a3 = 1
+a4 = 1
+alpha = 1
+rc = 1
+
+
+function Zl(r)
+   1 + (z-1)*exp(-a1*r)- r*(a3+a4*r)*exp(-a2*r)
+end
+
+function potential(r)
+   -Zl(r)/r-alpha/(2*r^4)*(1-exp(-(r/rc)^6))
+end
+
+function potentialH(r)
+   1/r
+end
+
+function angular(l,r)
+   l*(l+1)/(2*r^2)
+end
+
+function Sturmian(k,l,r)
+   ö = zeros(1,Max)
+   ö[k] = 1
+   sqrt(factorial(k)/(2*factorial(2*l+1+k)))*exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+1}(vec(ö))(2*r*Q)
+end
+
+function DSturmian(k,l,r)
+   if k <= 1
+      return 0
+   else
+      ö = zeros(1,Max)
+      ö[k] = 1
+      öö = zeros(1,Max)
+      öö[k-1] = 1
+      return sqrt(factorial(k)/(2*factorial(2*l+1+k)))*(-2*exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+2}(vec(öö))(2*r*Q)+2*Q*(1+l)*exp(-Q*r)*(2*Q*r)^(l)*Laguerre{2*l+1}(vec(ö))(2*r*Q)-Q*exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+1}(vec(ö))(2*r*Q))
    end
+end
 
-A = [1 2 3; 4 5 6; 6 6 6]
-det(A)
-f(3)
-print(f(30))
+O = zeros((Max,Max))
+Hamil1 = zeros((Max,Max))
+Hamil2 = zeros((Max,Max))
 
+for k1 = 1:Max
+   for k2 = k1:Max
+      O[k1,k2] = quadgk(r-> Sturmian(k1,1,r)*Sturmian(k2,1,r),0,1000)[1]
+      O[k2,k1] = O[k1,k2]
+      Hamil1[k1,k2] = quadgk(r-> Sturmian(k1,1,r)*Sturmian(k2,1,r)*(potentialH(r)+angular(1,r)),0,1000)[1]
+      Hamil1[k2,k1] = Hamil1[k1,k2]
+      Hamil2[k1,k2] = quadgk(r-> DSturmian(k1,1,r)*DSturmian(k2,1,r)/2,0,1000)[1]
+      Hamil2[k2,k1] = Hamil2[k1,k2]
+   end
+end
 
-
+Hamil = Hamil1 + Hamil2
+print(eigvals(Hamil,O))
