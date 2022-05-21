3efcc63a9f570b83e5268ce421cca878efe70764
Carlos Benavides-Riveros <cabenavir@gmail.com>
2022-05-21 13:07:58 +0200

    fisrt julia code

--- example.jl
+++ example.jl
@@ -1,7 +1,9 @@
 using LinearAlgebra, Polynomials, SpecialPolynomials
 using QuadGK, Calculus, CalculusWithJulia
+using GSL
+using Plots 
 
-Max = 25
+Max =10
 Q = 0.7
 z = 1
 a1 = 1
@@ -10,7 +12,7 @@ a3 = 1
 a4 = 1
 alpha = 1
 rc = 1
-L = 1
+L = 0
 
 function Zl(r)
    1 + (z-1)*exp(-a1*r)- r*(a3+a4*r)*exp(-a2*r)
@@ -29,38 +31,76 @@ function angular(l,r)
 end
 
 function Sturmian(k,l,r)
-   ö = zeros(1,Max)
-   ö[k] = 1
-   sqrt(factorial(big(k))/(2*factorial(big(2*l+1+k))))*exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+1}(vec(ö))(2*r*Q)
+   sqrt(factorial(k)/(2*factorial(2*l+1+k)))*exp(-Q*r)*(2*Q*r)^(l+1)*sf_laguerre_n(k,2*l+1,2*r*Q)
 end
 
 function DSturmian(k,l,r)
-   if k <= 1
-      return 0
+   if k < 1
+      return  sqrt(factorial(k)/(2*factorial(2*l+1+k)))*Q*(2*(1+l)*exp(-Q*r)*(2*Q*r)^(l)*sf_laguerre_n(k,2*l+1,2*r*Q)-exp(-Q*r)*(2*Q*r)^(l+1)*sf_laguerre_n(k,2*l+1,2*r*Q))
    else
-      ö = zeros(1,Max)
-      ö[k] = 1
-      öö = zeros(1,Max)
-      öö[k-1] = 1
-      return sqrt(factorial(big(k))/(2*factorial(big(2*l+1+k))))*Q*(-2*exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+2}(vec(öö))(2*r*Q)+2*(1+l)*exp(-Q*r)*(2*Q*r)^(l)*Laguerre{2*l+1}(vec(ö))(2*r*Q)-exp(-Q*r)*(2*Q*r)^(l+1)*Laguerre{2*l+1}(vec(ö))(2*r*Q))
+      return  sqrt(factorial(k)/(2*factorial(2*l+1+k)))*Q*(-2*exp(-Q*r)*(2*Q*r)^(l+1)*sf_laguerre_n(k-1,2*l+2,2*r*Q)+2*(1+l)*exp(-Q*r)*(2*Q*r)^(l)*sf_laguerre_n(k,2*l+1,2*r*Q)-exp(-Q*r)*(2*Q*r)^(l+1)*sf_laguerre_n(k,2*l+1,2*r*Q))
    end
 end
 
-O = zeros((Max,Max))
-Hamil1 = zeros((Max,Max))
-Hamil2 = zeros((Max,Max))
-
-for k1 = 1:Max
-   for k2 = k1:Max
-      O[k1,k2] = quadgk(r-> Sturmian(k1,L,r)*Sturmian(k2,L,r),0,100)[1]
-      O[k2,k1] = O[k1,k2]
-      Hamil1[k1,k2] = quadgk(r-> Sturmian(k1,L,r)*Sturmian(k2,L,r)*(potentialH(r)+angular(1,r)),0,100)[1]
-      Hamil1[k2,k1] = Hamil1[k1,k2]
-      Hamil2[k1,k2] = quadgk(r-> DSturmian(k1,L,r)*DSturmian(k2,L,r)/2,0,100)[1]
-      Hamil2[k2,k1] = Hamil2[k1,k2]
-      print(k1,k2)
-   end
+function Kinetic(k1,k2,L,r)
+   DSturmian(k1,L,r^2)*DSturmian(k2,L,r^2)/2*2*r
 end
 
-Hamil = Hamil1 + Hamil2
-print(eigvals(Hamil,O))
+function energy(k1,k2,L,r)
+   Sturmian(k1,L,r^2)*Sturmian(k2,L,r^2)*(potentialH(r^2)+angular(L,r^2))*2*r
+end
+
+function Overlap(k1,k2,L,r)
+   Sturmian(k1,L,r^2)*Sturmian(k2,L,r^2)*2*r
+end
+
+function loop(Max,L)
+   O = zeros((Max+1,Max+1))
+   Hamil1 = zeros((Max+1,Max+1))
+   Hamil2 = zeros((Max+1,Max+1)) 
+   for k1 = 0:Max
+      for k2 = k1:Max
+         O[k1+1,k2+1] = quadgk(r-> Overlap(k1,k2,L,r),0,50,rtol=1e-4)[1]
+         Hamil1[k1+1,k2+1] = quadgk(r-> energy(k1,k2,L,r),0,50,rtol=1e-4)[1]
+         Hamil2[k1+1,k2+1] = quadgk(r-> Kinetic(k1,k2,L,r),0,50,rtol=1e-4)[1]
+     end
+  end
+  for k1 = 0:Max
+     for k2 = k1:Max
+        O[k2+1,k1+1] = O[k1+1,k2+1]
+        Hamil1[k2+1,k1+1] = Hamil1[k1+1,k2+1]
+        Hamil2[k2+1,k1+1] = Hamil2[k1+1,k2+1]
+     end
+  end
+  Hamil = Hamil1 + Hamil2
+  print(eigvals(Hamil,O))
+end
+
+
+function loopNum(Max,L)
+   O = zeros((Max+1,Max+1))
+   Hamil1 = zeros((Max+1,Max+1))
+   Hamil2 = zeros((Max+1,Max+1)) 
+   x=0.01:0.01:50
+   for k1 = 0:Max
+      for k2 = k1:Max
+         O[k1+1,k2+1] = sum(Overlap.(k1,k2,L,x))*0.01
+         Hamil1[k1+1,k2+1] = sum(energy.(k1,k2,L,x))*0.01
+         Hamil2[k1+1,k2+1] = sum(Kinetic.(k1,k2,L,x))*0.01
+     end
+  end
+  for k1 = 0:Max
+     for k2 = k1:Max
+        O[k2+1,k1+1] = O[k1+1,k2+1]
+        Hamil1[k2+1,k1+1] = Hamil1[k1+1,k2+1]
+        Hamil2[k2+1,k1+1] = Hamil2[k1+1,k2+1]
+     end
+  end
+  Hamil = Hamil1 + Hamil2
+  print(eigvals(Hamil,O))
+end
+
+#loop(Max,L)
+#print("VVVVVVVVVVVVV")
+loopNum(Max,L)
+
